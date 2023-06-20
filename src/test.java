b9222bc22a0e7625c33e5b9842b915830214ba77
DavidJPLeRoux <djpleroux@gmail.com>
2023-06-20 01:42:19 -0400

    County Check 1.3
    Moved back to JDK 18.02

--- src/test.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.*;
-
-public class test {
-    public static void main(String[] args) {
-        CountyCheck.countyCheck("C:\\Users\\LEROUXDJ21\\Downloads\\test1.xls", "C:\\Users\\LEROUXDJ21\\Downloads\\results.xls", "ILLINOIS", "WILL", "BRAIDWOOD", new JPanel());
-    }
-}
